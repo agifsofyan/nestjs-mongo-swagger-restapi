a909f465df654945b2b5f136e84ba1e5355b1b22
AgifSofyan <alpenizme@gmail.com>
2020-09-11 17:15:38 +0700

    adding image upload documentation

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -5,19 +5,39 @@ import {
     UseInterceptors,
     UploadedFile,
     UploadedFiles,
+    UseGuards,
     Res,
     Param,
 } from '@nestjs/common';
   
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import { ApiTags, ApiOperation, ApiHeader, ApiQuery } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 import { editFileName, imageFileFilter } from './helper/file-upload.helper';
-  
+
+@ApiTags('Uploads')
+@UseGuards(RolesGuard)
 @Controller('uploads')
 export class MediaController {
     
+    /**
+     * @route   POST /api/v1/uploads
+     * @desc    Upload Image
+     * @access  Public
+     */
     @Post()
+    @UseGuards(AuthGuard('jwt'))
+    @Roles('Administrator')
+    @ApiOperation({ summary: 'Single Upload' })
+    @ApiHeader({
+      name: 'x-auth-token',
+      description: 'token.'
+    })
+  
     @UseInterceptors(
       FileInterceptor('image', {
         storage: diskStorage({
@@ -40,6 +60,19 @@ export class MediaController {
     }
   
     @Post('multiple')
+    /**
+	 * @route   POST /api/v1/uploads/multiple
+	 * @desc    Multiple Image Upload
+	 * @access  Public
+	 */
+    @UseGuards(AuthGuard('jwt'))
+    @Roles('Administrator')
+    @ApiOperation({ summary: 'Multiple Upload' })
+    @ApiHeader({
+      name: 'x-auth-token',
+      description: 'token.'
+    })
+  
     @UseInterceptors(
       FilesInterceptor('image', 20, {
         storage: diskStorage({
@@ -60,7 +93,19 @@ export class MediaController {
       });
       return response;
     }
-  
+    
+    /**
+	 * @route   POST /api/v1/uploads/:new name of image
+	 * @desc    Get Image [:imgpath]
+	 * @access  Public
+	 */
+    @UseGuards(AuthGuard('jwt'))
+    @Roles('Administrator')
+    @ApiOperation({ summary: 'Get Image' })
+    @ApiHeader({
+      name: 'x-auth-token',
+      description: 'token.'
+    })
     @Get(':imgpath')
     seeUploadedFile(@Param('imgpath') image, @Res() res) {
       return res.sendFile(image, { root: './storage/images' });
--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MediaController } from './media.controller';
 import { MulterModule } from '@nestjs/platform-express';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [MulterModule.register({
     dest: './storage/images',
-  })],
+  }),
+    AuthModule,
+  ],
   controllers: [MediaController]
 })
 export class MediaModule {}
